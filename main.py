b9a7859c5e0a639ea98caeab19bda9b7fbc807d9
Nikita <nikitamolavko27@gmail.com>
2023-12-04 09:28:42 +0100

    První úprava

---
+++ main.py
@@ -0,0 +1,24 @@
+
+print ("Sčítani")
+
+print (3+3)
+
+print ("Odčítání")
+
+print (3-3)
+
+print ("Nasobení")
+
+print (3*3)
+
+print ("Dělení")
+
+print (3/3)
+
+print ("Odmocnění")
+
+print (121**(1/12))
+
+print ("Mocnění")
+
+print (3**3)
